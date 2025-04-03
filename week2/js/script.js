531215cb18c3c7fe6e4ffee3ef70b34af913eab2
marii-bit <mawishart@email.neit.edu>
2025-04-03 10:22:18 -0400

    Added new fils

---
+++ week2/js/script.js
@@ -0,0 +1,5 @@
+// Example of JavaScript file 
+
+var name = prompt("What is your name???")
+document.getElementById("greeting").innerHTML=`Hello ${name} how are you today?`;
+document.getElementById("greeting").style.color = "red";
